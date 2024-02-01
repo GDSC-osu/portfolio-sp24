229e48cc0cdb7f152eda87c4c3875fe42cdd8a59
rahulmedicharla <rmedicharla@gmail.com>
2024-01-31 21:50:15 -0500

    working auth flow

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,24 @@ import theme from './theme'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Contact from './pages/Contact'
 
+//firebase imports
+import { initializeApp } from "firebase/app";
+import { initializeAuth } from "firebase/auth";
+  
+
 function App() {
+  // intialize firebase app
+  const firebaseConfig = {
+    apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID
+  }
+  const app = initializeApp(firebaseConfig);
+  initializeAuth(app);
+
   return (
     /* Everything is rendered within Material-ui's ThemeProvider, which gives styling to its elements */
     <ThemeProvider theme={theme}>
